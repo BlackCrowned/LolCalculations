7c5b5adb1b6cae98e766dfe2333a356063f3f773
Michael Moehrle <mi.moehrle@gmail.com>
2014-01-26 02:42:19 +0100

    Version #1

---
+++ Damage Calculation/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="style/style_1.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+		<title>Damage Calculation</title>
+	</head>
+
+	<body>
+
+		<div class="Champions">
+			Add Champion
+		</div>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="style/style_1.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+		<script type="text/javascript" src="../CWeb/cWeb.js"></script>
+		<script type="text/javascript" src="javascript/javascript_1.js"></script>
+		<title>LoLCalculations</title>
+	</head>
+	<body>
+		<h1>LoLCalculations</h1>
+
+		<h2>What do you want to calculate?</h2>
+
+		<div class="Selectables">
+
+			<div class="Selectable" id="DamageCalc">
+				<a href="Damage Calculation/index.php"> Damage Calculation </a>
+			</div>
+			
+
+		</div>
+	</body>
+</html>
